b3ee12c9b3e92efab191e2b48b04a5be44b9e825
Charl Möller <charl@healthqtech.com>
2018-09-05 12:41:05 +0200

    Allow use of 'external' mechanism in application configuration

--- types/mech.pp
+++ types/mech.pp
@@ -1,2 +1,2 @@
 # @since 2.0.0
-type SASL::Mech = Enum['anonymous', 'cram-md5', 'digest-md5', 'login', 'ntlm', 'plain']
+type SASL::Mech = Enum['anonymous', 'cram-md5', 'digest-md5', 'login', 'ntlm', 'plain', 'external']
